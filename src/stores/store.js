70b9723114b423d09f591ee635fc6a56a13191f9
Berkan <yusufbk92@gmail.com>
2025-09-26 02:12:58 +0300

    updates

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const UseStore = defineStore("UseStore", {
   state: () => ({
     WebAppName: 'RouteWise',
-    AppVersion:'1.0.7',
+    AppVersion:'1.0.8',
     Config: {},
     UserData: {},
 
