3e5a7a0b574d3828a773f15dcd14b97b90c12fc7
Berkan <yusufbk92@gmail.com>
2025-10-23 10:49:10 +0300

    Add SPA rewrite rule for Vercel

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const UseStore = defineStore("UseStore", {
   state: () => ({
     WebAppName: 'RouteWise',
-    AppVersion:'1.2.6',
+    AppVersion:'1.2.7',
     Config: {},
     UserData: {},
 
